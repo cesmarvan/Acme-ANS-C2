cfa50e1c841bd95d93c061ec9917e40226bc161b
jotageerre <jotageerre04@gmail.com>
2025-03-13 12:57:01 +0100

    Arreglos de organización del customer y de los datos de prueba

--- src/main/java/acme/realms/Customer.java
+++ src/main/java/acme/realms/Customer.java
@@ -1,9 +1,9 @@
 
-package acme.entities.customer;
+package acme.realms;
 
 import javax.persistence.Entity;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -15,7 +15,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Customer extends AbstractEntity {
+public class Customer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
