1a250e7c4a33241778bdd55bb9521efbc3c83173
German220203 <gerojegar@alum.us.es>
2025-04-02 19:45:07 +0200

    G 12 incompleto

---
+++ src/main/java/acme/features/administrator/airline/AdministratorAirlineListService.java
@@ -0,0 +1,45 @@
+
+package acme.features.administrator.airline;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airline.Airline;
+
+@GuiService
+public class AdministratorAirlineListService extends AbstractGuiService<Administrator, Airline> {
+
+	@Autowired
+	private AdministratorAirlineRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		List<Airline> airlines;
+
+		airlines = this.repository.findAllAirlines();
+
+		super.getBuffer().addData(airlines);
+	}
+
+	@Override
+	public void unbind(final Airline airline) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(airline, "name", "iataCode", "website");
+		super.addPayload(dataset, airline, "name");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
---
+++ src/main/java/acme/features/administrator/airline/AdministratorAirlineUpdateService.java
@@ -0,0 +1,85 @@
+
+package acme.features.administrator.airline;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airline.Airline;
+import acme.entities.airline.AirlineType;
+
+@GuiService
+public class AdministratorAirlineUpdateService extends AbstractGuiService<Administrator, Airline> {
+
+	// Internal state ------------------------------------
+
+	@Autowired
+	private AdministratorAirlineRepository repository;
+
+	// AbstractGuiService interface
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Airline airline;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		airline = this.repository.findAirlineById(id);
+
+		super.getBuffer().addData(airline);
+	}
+
+	@Override
+	public void bind(final Airline airline) {
+		super.bindObject(airline, "name", "iataCode", "website", "type", "foundationMoment", "email", "phoneNumber");
+	}
+
+	@Override
+	public void validate(final Airline airline) {
+		{
+			boolean confirmation = super.getRequest().getData("confirmation", boolean.class);
+			super.state(confirmation, "confirmation", "acme.validation.airline.no-confirmation.message");
+		}
+		{
+			boolean uniqueAirline;
+			Airline otherAirline;
+
+			otherAirline = this.repository.findAirlineByIATA(airline.getIataCode());
+			uniqueAirline = otherAirline == null || otherAirline.equals(airline);
+
+			super.state(uniqueAirline, "iataCode", "acme.validation.airline.duplicated-iata-code.message");
+		}
+	}
+
+	@Override
+	public void perform(final Airline airline) {
+		this.repository.save(airline);
+	}
+
+	@Override
+	public void unbind(final Airline airline) {
+		Dataset dataset;
+		boolean confirmation = true;
+
+		SelectChoices choices;
+		choices = SelectChoices.from(AirlineType.class, airline.getType());
+
+		dataset = super.unbindObject(airline, "name", "iataCode", "website", "type", "foundationMoment", "email", "phoneNumber");
+		dataset.put("confirmation", confirmation);
+
+		dataset.put("types", choices);
+
+		super.getResponse().addData(dataset);
+
+	}
+
+}
